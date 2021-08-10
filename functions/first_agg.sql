13358a2575a0113e1219c77ba6b768bbb7e966f0
Alex Theodore <12698112+alexitheodore@users.noreply.github.com>
2021-08-09 17:10:10 -0700

    refactor:
    Makes the first() agg function compatible with pg11.

--- functions/first_agg.sql
+++ functions/first_agg.sql
@@ -9,10 +9,20 @@ STRICT
 PARALLEL SAFE
 ;
 
+
+/* Until pg13 becomes standard, the "OR REPLACE" syntax cannot be used and this silly workaround is necessary. */
+-->
+\out /dev/null
+SELECT to_regproc('first') IS NULL AS needed;
+\gset \out \if :needed
+--<
+
 -- And then wrap an aggregate around it
-CREATE OR REPLACE AGGREGATE first (
+CREATE AGGREGATE first (
 	sfunc = first_agg
 ,	basetype = anyelement
 ,	stype = anyelement
 )
 ;
+
+\endif
